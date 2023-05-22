6be7951aa103a603de5a1e8b5bf02c218b4dcb5b
sbojorge <sara.bojorge@gmail.com>
2023-05-22 17:30:12 +0200

    Add profile serializer and list view

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'rest_framework',
 
     'profiles',
 
---
+++ profiles/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import ProfileList
+
+urlpatterns = [
+    path('profiles/', ProfileList.as_view(),)
+]
