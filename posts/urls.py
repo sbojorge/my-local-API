310e3e1d12cc8a7ce284f1dd33a0e28eed34e5a1
sbojorge <sara.bojorge@gmail.com>
2023-05-26 16:19:52 +0200

    Create the PostDetail view with GET and PUT methods

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostList 
+from .views import PostList, PostDetail
 
 urlpatterns = [
     path('posts/', PostList.as_view()),
+    path('posts/<int:pk>/', PostDetail.as_view()),
    
 ]
