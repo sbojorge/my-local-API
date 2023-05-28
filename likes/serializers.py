76f54f3eaf45ab7306bc2b1945e9a8a793eb6a56
sbojorge <sara.bojorge@gmail.com>
2023-05-28 21:46:35 +0200

    Add handling duplicate functionality in the likes app

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Like
-
+from django.db import IntegrityError
 
 class LikeSerializer(serializers.ModelSerializer):
    """
@@ -12,5 +12,14 @@ class LikeSerializer(serializers.ModelSerializer):
    class Meta:
       model = Like
       fields = ['id', 'owner', 'post', 'created_at']
+
+   def create(self, validated_data):
+      try:
+         return super().create(validated_data)
+      except IntegrityError:
+         serializers.ValidationError({
+            'detail': 'possible duplicate'
+         })
+         
 
   
