fea580eb62f2f1d06f455a52a28f03a0c5305320
sbojorge <sara.bojorge@gmail.com>
2023-05-28 22:22:51 +0200

    Add the RetrieveDestroy like by id functionality

---
+++ likes/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import LikeList, LikeDetail
+
+urlpatterns = [
+    path('likes/', LikeList.as_view()),
+    path('likes/<int:pk>/', LikeDetail.as_view()),
+]
