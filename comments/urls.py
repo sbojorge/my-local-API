a76272596ee39e03d1e8a286cb1cbbddf7196253
sbojorge <sara.bojorge@gmail.com>
2023-05-28 11:58:22 +0200

    Create generic views CommentList and CommentDetail

---
+++ comments/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import CommentList, CommentDetail
+
+urlpatterns = [
+    path('comments/', CommentList.as_view()),
+    path('comments/<int:pk>/', CommentDetail.as_view()),
+]
