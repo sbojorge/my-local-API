70c16a4fcb8663b468bb21c3a3a31b136a8b0e28
sbojorge <sara.bojorge@gmail.com>
2023-05-29 08:10:02 +0200

    Refactor the ProfileList and ProfileDetail views

--- profiles/views.py
+++ profiles/views.py
@@ -1,53 +1,35 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
 from .models import Profile
 from .serializers import ProfileSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(APIView):
+class ProfileList(generics.ListAPIView):
     """
     List all profiles
     No Create view (post method), as profile creation handled by django signals
     """
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
 
 
-class ProfileDetail(APIView):
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve a profile or edit it if you own it
+    """
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.all()
 
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
+
+
+        
+         
+
+
+
+
     
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, context={'request': request}
-        )
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
