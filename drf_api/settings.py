6fc8d81bb2191ec51223ac8dae9ab442a108b416
sbojorge <sara.bojorge@gmail.com>
2023-06-21 22:02:10 +0200

    restore the allowed host value in settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -56,7 +56,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    os.environ.get('ALLOWED_HOST'),
+     os.environ.get('ALLOWED_HOST'),
     'localhost',
 ]
 
